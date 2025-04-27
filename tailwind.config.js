d5f299190312a36df21ba57c59b1590f9e9ce8a0
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2025-04-28 03:16:01 +0530

    fe

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          geist: ['Giest Fallback', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
